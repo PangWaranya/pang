f468a4dd92c203530ff37f558a71276179cf5e6b
PangWaranya <watanya.p12@gmail.com>
2020-12-04 17:14:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
